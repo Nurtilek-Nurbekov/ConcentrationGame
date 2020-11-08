4a53fc863955aa2ce33377d4fd8829b2c0c7cdaf
nurtSTR <44082568+nurtSTR@users.noreply.github.com>
2020-11-08 22:20:15 +0600

    Initial commit

--- ConcentrationGameFullVersion/ViewController.swift
+++ ConcentrationGameFullVersion/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    lazy var game = ConcentrationGame(numberOfPairsOfCards: (buttonCollection.count + 1) / 2)
+    
+    var emojiCollection = ["🍏","🍎","🍐","🍊","🍋","🍌","🍉","🍇","🍓","🍈","🍒","🍑"]
+        
+    var emojiDictionary = [Int:String]()
+    
+    func emojiIdentifier(for card: Card) -> String {
+        if emojiDictionary[card.identifier] == nil {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiCollection.count)))
+            emojiDictionary[card.identifier] = emojiCollection.remove(at: randomIndex)
+        }
+        return emojiDictionary[card.identifier] ?? "?"
     }
-
+    
+    func updateViewFromModel() {
+        for index in buttonCollection.indices {
+            let button = buttonCollection[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emojiIdentifier(for: card), for: .normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) : #colorLiteral(red: 0.3450980392, green: 0.337254902, blue: 0.8392156863, alpha: 1)
+            }
+        }
+    }
+    
+    @IBOutlet var buttonCollection: [UIButton]!
+    @IBAction func buttonAction (_ sender: UIButton) {
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            game.chooseCard(at: buttonIndex)
+            updateViewFromModel()
+        }
+    }
 
 }
 
